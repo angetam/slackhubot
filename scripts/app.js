537c6adbf3eea73a74e8f0d80e710ffb5e458b07
angetam <angetam@gmail.com>
2017-02-26 18:05:18 -0800

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+module.exports = function(bot) {
+    var greeting = ['hi', 'howdy', 'hello', 'good morning'];
+    var adjective = ['Great!', 'Awesome.', 'You got it.', 'Excellent.'];
+    var images = ["https://cdn.meme.am/cache/instances/folder270/56977270.jpg", "http://combiboilersleeds.com/images/you-can-do-it/you-can-do-it-9.jpg", "http://combiboilersleeds.com/image.php?pic=/images/you-can-do-it/you-can-do-it-7.jpg"]
+    var toDo = [];
+    bot.respond(greeting, function(msg) {
+        return msg.send("Hi there! What did you need to do today? Type 'add' followed by your task");
+    })
+    bot.hear(/add (.*)/i, function(msg){
+        toDo.push(msg.match[1]);
+        return msg.send(msg.random(adjective) + ' I\'ve added ' + msg.match[1] + ' to your list. Anything else? \n\nTo check what you have so far, type "check"');   
+    })
+    bot.hear('no', function(msg){
+            return msg.send(msg.random(adjective) + " Here are all the things you  need to do: " + toDo + ". " + msg.random(images));
+        })
+    bot.hear('check', function(msg) {
+        return msg.send('To Do List: ' + toDo)
+    })
+}
