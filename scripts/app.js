80651a62fe1c89b1ce70953f489f91a39d107287
angetam <angetam@gmail.com>
2017-02-27 17:49:43 -0800

    add if else statement

--- scripts/app.js
+++ scripts/app.js
@@ -3,17 +3,24 @@ module.exports = function(bot) {
     var adjective = ['Great!', 'Awesome.', 'You got it.', 'Excellent.'];
     var images = ["https://cdn.meme.am/cache/instances/folder270/56977270.jpg", "http://combiboilersleeds.com/images/you-can-do-it/you-can-do-it-9.jpg", "http://combiboilersleeds.com/image.php?pic=/images/you-can-do-it/you-can-do-it-7.jpg"]
     var toDo = [];
-    bot.respond(greeting, function(msg) {
+    bot.reply(greeting, function(msg) {
         return msg.send("Hi there! What did you need to do today? Type 'add' followed by your task");
     })
     bot.hear(/add (.*)/i, function(msg){
         toDo.push(msg.match[1]);
-        return msg.send(msg.random(adjective) + ' I\'ve added ' + msg.match[1] + ' to your list. Anything else? \n\nTo check what you have so far, type "check"');   
+        return msg.send(msg.random(adjective) + ' I\'ve added ' + msg.match[1] + ' to your list. Anything else? \n\nTo check what you have so far, type "check"');
     })
+    bot.hear('yes', function(msg){
+            return msg.send(msg.random(adjective) + " What else should I add?");
+        })
     bot.hear('no', function(msg){
             return msg.send(msg.random(adjective) + " Here are all the things you  need to do: " + toDo + ". " + msg.random(images));
         })
     bot.hear('check', function(msg) {
-        return msg.send('To Do List: ' + toDo)
+        if (toDo.length>3) {
+            return msg.send('To Do List: ' + toDo + '\nYou\'re a busy bee!')
+        } else {
+            return msg.send('To Do List: ' + toDo + '\nLet me know if there\'s anything else.')
+        }
     })
 }
